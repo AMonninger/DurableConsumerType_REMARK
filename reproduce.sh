8ffa3d3762c59c3780f2cde6f9d7e2cd084295e8
AMonninger <66739352+AMonninger@users.noreply.github.com>
2023-01-23 12:45:35 -0500

    REMARK_public

--- reproduce.sh
+++ reproduce.sh
@@ -4,7 +4,5 @@ python3 -m pip install --quiet -r requirements.txt
 
 # navigate to code/ and execute the python file to create figures
 cd ./code/python
-jupyter nbconvert --to python REMARK-starter-example.ipynb
-ipython REMARK-starter-example.py
-
-
+jupyter nbconvert --to python DurableModel_Notebook.ipynb
+ipython DurableModel_Notebook.py
