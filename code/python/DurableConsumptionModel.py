92448232b3c3d5019e6f71f8a20edbfd92e977cb
Alan Lujan <alanlujan91@gmail.com>
2023-11-22 09:54:10 -0500

    np.int deprecated
    replace with np.int64

--- code/python/DurableConsumptionModel.py
+++ code/python/DurableConsumptionModel.py
@@ -692,7 +692,7 @@ class DurableConsumptionModelClass(ModelClass):
             sim.n = np.zeros(sim_shape)
             sim.n1 = np.zeros((0, 0))  # not used
             sim.n2 = np.zeros((0, 0))  # not used
-        sim.discrete = np.zeros(sim_shape, dtype=np.int)
+        sim.discrete = np.zeros(sim_shape, dtype=np.int64)
         if par.do_2d:
             sim.d = np.zeros((0, 0))  # not used
             sim.d1 = np.zeros(sim_shape)
