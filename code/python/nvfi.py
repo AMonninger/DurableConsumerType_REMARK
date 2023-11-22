65e2c194e3ad06e3a94953d244c8ae6ccaa4e293
Alan Lujan <alanlujan91@gmail.com>
2023-11-22 09:35:11 -0500

    clean up and format

--- code/python/nvfi.py
+++ code/python/nvfi.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import njit, prange
 
 # consav
-from consav import linear_interp # for linear interpolation
+from consav import linear_interp  # for linear interpolation
 from consav import golden_section_search
 
 # local modules
@@ -12,20 +12,21 @@ import utility
 # keep #
 ########
 
+
 @njit
-def obj_keep(c,n,m,inv_w,grid_a,d_ubar,alpha,rho):
-    """ evaluate bellman equation """
+def obj_keep(c, n, m, inv_w, grid_a, d_ubar, alpha, rho):
+    """evaluate bellman equation"""
 
     # a. end-of-period assets
-    a = m-c
-    
+    a = m - c
+
     # b. continuation value
-    w = -1.0/linear_interp.interp_1d(grid_a,inv_w,a)
+    w = -1.0 / linear_interp.interp_1d(grid_a, inv_w, a)
 
     # c. total value
-    value_of_choice = utility.func_nopar(c,n,d_ubar,alpha,rho) + w
+    value_of_choice = utility.func_nopar(c, n, d_ubar, alpha, rho) + w
 
-    return -value_of_choice # we are minimizing
+    return -value_of_choice  # we are minimizing
 
 
 # @njit(parallel=True)
@@ -47,20 +48,20 @@ def obj_keep(c,n,m,inv_w,grid_a,d_ubar,alpha,rho):
 #     # loop over outer states
 #     for i_p in prange(par.Np):
 #         for i_n in range(par.Nn):
-            
+
 #             # outer states
 #             n = par.grid_n[i_n]
 
 #             # loop over m state
 #             for i_m in range(par.Nm):
-                
+
 #                 # a. cash-on-hand
 #                 m = par.grid_m[i_m]
 #                 if i_m == 0:
 #                     c[i_p,i_n,i_m] = 0
 #                     inv_v[i_p,i_n,i_m] = 0
 #                     if par.do_marg_u:
-#                         inv_marg_u[i_p,i_n,i_m] = 0        
+#                         inv_marg_u[i_p,i_n,i_m] = 0
 #                     continue
 
 #                 # b. optimal choice
@@ -78,21 +79,25 @@ def obj_keep(c,n,m,inv_w,grid_a,d_ubar,alpha,rho):
 # adj #
 #######
 
+
 @njit
-def obj_adj(d,x,inv_v_keep,grid_n,grid_m):
-    """ evaluate bellman equation """
+def obj_adj(d, x, inv_v_keep, grid_n, grid_m):
+    """evaluate bellman equation"""
 
     # a. cash-on-hand
-    m = x-d
+    m = x - d
 
     # b. durables
     n = d
-    
+
     # c. value-of-choice
-    return -linear_interp.interp_2d(grid_n,grid_m,inv_v_keep,n,m)  # we are minimizing
+    return -linear_interp.interp_2d(
+        grid_n, grid_m, inv_v_keep, n, m
+    )  # we are minimizing
+
 
 @njit(parallel=True)
-def solve_adj(t,sol,par):
+def solve_adj(t, sol, par):
     """solve bellman equation for adjusters using nvfi"""
 
     # unpack output
@@ -112,29 +117,39 @@ def solve_adj(t,sol,par):
 
     # loop over outer states
     for i_p in prange(par.Np):
-            
         # loop over x state
         for i_x in range(par.Nx):
-            
             # a. cash-on-hand
             x = par.grid_x[i_x]
             if i_x == 0:
-                d[i_p,i_x] = 0
-                c[i_p,i_x] = 0
-                inv_v[i_p,i_x] = 0
+                d[i_p, i_x] = 0
+                c[i_p, i_x] = 0
+                inv_v[i_p, i_x] = 0
                 if par.do_marg_u:
-                    inv_marg_u[i_p,i_x] = 0        
+                    inv_marg_u[i_p, i_x] = 0
                 continue
 
             # b. optimal choice
-            d_low = np.fmin(x/2,1e-8)
-            d_high = np.fmin(x,par.n_max)
-            d[i_p,i_x] = golden_section_search.optimizer(obj_adj,d_low,d_high,args=(x,inv_v_keep[i_p],grid_n,grid_m),tol=par.tol)
+            d_low = np.fmin(x / 2, 1e-8)
+            d_high = np.fmin(x, par.n_max)
+            d[i_p, i_x] = golden_section_search.optimizer(
+                obj_adj,
+                d_low,
+                d_high,
+                args=(x, inv_v_keep[i_p], grid_n, grid_m),
+                tol=par.tol,
+            )
 
             # c. optimal value
-            m = x - d[i_p,i_x]
-            c[i_p,i_x] = linear_interp.interp_2d(par.grid_n,par.grid_m,c_keep[i_p],d[i_p,i_x],m)
-            inv_v[i_p,i_x] = -obj_adj(d[i_p,i_x],x,inv_v_keep[i_p],grid_n,grid_m)
+            m = x - d[i_p, i_x]
+            c[i_p, i_x] = linear_interp.interp_2d(
+                par.grid_n, par.grid_m, c_keep[i_p], d[i_p, i_x], m
+            )
+            inv_v[i_p, i_x] = -obj_adj(d[i_p, i_x], x, inv_v_keep[i_p], grid_n, grid_m)
             if par.do_marg_u:
-                inv_marg_u[i_p,i_x] = 1/utility.marg_func_nopar(c[i_p,i_x],d[i_p,i_x],d_ubar,alpha,rho)
+                inv_marg_u[i_p, i_x] = 1 / utility.marg_func_nopar(
+                    c[i_p, i_x], d[i_p, i_x], d_ubar, alpha, rho
+                )
+
+
 #    print(c-d)
